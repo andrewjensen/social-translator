431f2a34f2f62b3085335e6a652854ce4f4ca78e
Skyler Mitchell <skylrmitchll@gmail.com>
2013-12-09 17:48:38 -0700

    Added to the question page

--- controllers/questions.js
+++ controllers/questions.js
@@ -5,16 +5,21 @@ exports.translationPage = function(req, res) {
 
 	var questionID = req.params.questionID;
 
-	//TODO: plug into the MongoDB for this
-	var question = {
-		questionID	: questionID
-	};
+	console.log('questionID: ' + questionID);
+	
+	Question.findOne({_id: questionID}, function(err, question) {
+		if (err)
+			res.send(err);	//TODO: handle error better
+		console.log('hello world');
+		console.log(question);
 
-	var pageData = {
-		title		: 'Question | Social Translator',
-		question	: question
-	};
-	res.render('question', pageData);
+		var pageData = {
+			title		: 'Question | Social Translator',
+			question	: question
+		};
+		res.render('question', pageData);
+
+	});
 
 };
 
--- models/question.js
+++ models/question.js
@@ -2,10 +2,15 @@
  * Question model
  */
 var mongoose = require('mongoose');
+var ObjectId = mongoose.ObjectId;
 
 var questionSchema = new mongoose.Schema({
 	text	: String,
-	context	: String
+	context	: String,
+	timestamp: String,
+	comments: [ObjectId],
+	tags: [ObjectId],
+	score: String
 });
 
 module.exports = mongoose.model('questions', questionSchema);
