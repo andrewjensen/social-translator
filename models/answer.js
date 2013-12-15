abe7e9792344a4eb141c6a5fb38c0164186badf6
Andrew Jensen <andrewjensen90@gmail.com>
2013-12-14 17:14:02 -0700

    Added a “question” field to answers. Now it’s really easy to lookup questions based on their answers.

--- models/answer.js
+++ models/answer.js
@@ -5,17 +5,18 @@ var mongoose = require('mongoose');
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
 var answerSchema = new mongoose.Schema({
-	author          : {type: ObjectId, ref: 'users'},
-	translation 	: String,
+	author			: {type: ObjectId, ref: 'users'},
+	question		: {type: ObjectId, ref: 'questions'},
+	translation		: String,
 	supplementary	: String,
 	upvotes			: Number,
 	downvotes		: Number,
 	score			: Number,
-	timestamp   	: Number,
-	comments 	    : [{
-		text        : String,
-		timestamp   : Number,
-		userID      : {type: ObjectId, ref: 'users'}
+	timestamp		: Number,
+	comments		: [{
+		text		: String,
+		timestamp	: Number,
+		userID		: {type: ObjectId, ref: 'users'}
 	}]
 });
 
